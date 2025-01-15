0e6bcf508f16b77388aac6fdae2a02c805f465cc
HilqiaKenda <hilkiakenda@gmail.com>
2025-01-15 13:20:44 +0300

    sort name games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ interface selectGenreProps {
 const GameGrid = ({ gameQuery }: selectGenreProps) => {
   const { data, error, isLoading } = useGames(gameQuery);
 
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,17 +1,39 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface SortOrderProps {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: SortOrderProps) => {
+  const sortOrders = [
+    { value: "", label: "Revelance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Aveage Rating" },
+  ];
+
+  const currentOrder = sortOrders.find((order) => order.value === sortOrder);
+
   return (
     <Menu>
       <>
         <MenuButton as={Button} rightIcon={<BsChevronDown />} paddingX={5}>
-          Order by: revelance
+          Order by: {currentOrder?.label || "Relevance"}
         </MenuButton>
         <MenuList>
-          <MenuItem>item 1</MenuItem>
-          <MenuItem>item 2</MenuItem>
-          <MenuItem>item 3</MenuItem>
+          {sortOrders.map((order) => (
+            <MenuItem
+              key={order.value}
+              value={order.value}
+              onClick={() => onSelectSortOrder(order.value)}
+            >
+              {order.label}
+            </MenuItem>
+          ))}
         </MenuList>
       </>
     </Menu>
