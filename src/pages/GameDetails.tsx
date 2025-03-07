3a9b92a2ed33b180b0f9b1c20f0cdbe24b7326e2
hilqiaKenda <hilkiakenda@gmail.com>
2025-03-07 17:25:36 +0300

    working on game details layout

--- src/pages/GameDetails.tsx
+++ src/pages/GameDetails.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Heading, Spinner, SimpleGrid, GridItem } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
@@ -13,13 +13,17 @@ const GameDetails = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshot gameId={game.slug} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshot gameId={game.slug} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
